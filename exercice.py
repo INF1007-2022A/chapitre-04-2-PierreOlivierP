b28ac280e0cdc082af10da0c85768dc80b55809d
PierreOlivierP <pierre-olivier.piquant@polymtl.ca>
2022-10-17 19:03:03 -0400

    cubik

--- exercice.py
+++ exercice.py
@@ -4,16 +4,42 @@
 import random
 
 def get_first_part_of_name(name):
-	return ""
-
+	i = 0
+	for j in name:
+		if j == '-':
+			break
+		i+=1
+	bon = name[0:i]
+	bon = bon.lower()
+	return bon.capitalize()
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	def aa():
+		return random.randrange(0, 3)
+	return f"Aujourd’hui, j’ai vu un {animals[aa()]} s’emparer d’un panier `{adjectives[aa()]}` plein de {fruits[aa()]}."
 
 def encrypt(text, shift):
-	return ""
+	text = text.upper()
+	new = ""
+	for i in text:
+		if (ord(i) <= 90) and (ord(i) >= 90-26):
+			if ord(i) > 90-shift:
+				i = chr(ord(i)-26)
+			new += chr(ord(i)+shift)
+		else:
+			new += i
+	return new
 
 def decrypt(encrypted_text, shift):
-	return ""
+	text = encrypted_text.upper()
+	new = ""
+	for i in text:
+		if (ord(i) >= 90-26) and (ord(i) <= 90):
+			if ord(i) <= 90 - 26 + shift:
+				i = chr(ord(i) + 26)
+			new += chr(ord(i) - shift)
+		else:
+			new += i
+	return new
 
 
 if __name__ == "__main__":
